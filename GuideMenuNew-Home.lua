af8d2099f085e3e30c8c61a0944c10068f689f9d
bizkut <hasan@localhost.my>
2020-11-27 18:40:42 +0800

    first commit. Release: 8.0.23819

---
+++ GuideMenuNew-Home.lua
@@ -0,0 +1,19 @@
+local name,ZGV = ...
+
+-- #GLOBALS ZygorGuidesViewer
+
+local GuideMenu = ZGV.GuideMenu
+
+GuideMenu.HomeVersion = 1
+GuideMenu.Home={
+	{"banner", image=ZGV.DIR.."\\Skins\\banner"},
+	{"section", text=[[|cffffcc00Leveling|r.]]},
+	{"section", text=[[|cffffcc00Both|r.]]},
+	{"list", text=[[|cffffcc00Added Bastion (Story Only) - Polished|r.]]},
+	{"list", text=[[|cffffcc00Added Maldraxxus (Story Only) - Unpolished (In a beta state, will be polished in the next 1-2 days)|r.]]},
+	{"list", text=[[|cffffcc00Added Ardenweald (Story Only) - Unpolished (In a beta state, will be polished in the next 1-2 days)|r.]]},
+	{"list", text=[[|cffffcc00Added Revendreth (Story Only) - Unpolished (In a beta state, will be polished in the next 1-2 days)|r.]]},
+}
+
+
+-- faction="Alliance" {"separator"},
---
+++ Guides/Gold/ZygorGatheringCommonSHADOW.lua
@@ -0,0 +1,6 @@
+ZGV.Gold.guides_loaded=true
+--@@ONLYDEVSTART
+ZGV.DevEnd()
+--@@ONLYDEVEND
+
+ZygorGuidesViewer.GuideMenuTier = "SHA"
